11fcdf9b340563a59d5d6488f9759a9d377dd586
Andrew Giessel <andrew@growthsignals.com>
2025-02-15 10:22:53 -0500

    adding example ipython notebook and data directory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,7 +5,12 @@ import os
 import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+DATA_README = """# Local Data Directory
 
+Use this directory to keep data sources that you might reference as part
+of your project, but don't want to version control.  Typical examples might
+be a set of documents, csv files, or outputs of analyses.
+"""
 
 def remove_file(filepath: str) -> None:
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
@@ -32,4 +37,10 @@ if __name__ == "__main__":
 
     if "{{cookiecutter.dockerfile}}" != "y":
         remove_file("Dockerfile")
+
+    # create data directory and readme
+    # we do it here because we explicitly git ignore the data directory
+    os.mkdir(os.path.join(PROJECT_DIRECTORY, "data"))
+    with open(os.path.join(PROJECT_DIRECTORY, "data", "README.md"), "w") as f:
+        f.write(DATA_README)
 
